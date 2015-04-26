c7969dfae16be71925a3a16402b94c2e1b0829ab
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-04-27 02:11:55 +1200

    Largest of four solved

--- Bonfires/code.js
+++ Bonfires/code.js
@@ -28,5 +28,19 @@ var str = prompt("Please enter a sentence", "I'm a little tea pot");
 		titleCase=titleCase+strArray[pos][0].toUpperCase()+strArray[pos].substring(1).toLowerCase();
 	}
   alert(titleCase);
+	}
+
+function Solution7(){
 	
+var arr = [[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]];
+var largest=0;
+for (var i = 0; i < 4; i++) {
+	for (var j=0; j <4; j++){
+		if(arr[i][j]>largest)
+			largest=arr[i][j];
+	};
+	arr[i]=largest;
+	largest=0;
+};
+  alert(arr);
 }
