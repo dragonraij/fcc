f08dc7a6f3bf3196bf4cb7412463338b629e54e8
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-04-27 01:34:20 +1200

    Title Case sentence

--- Bonfires/code.js
+++ Bonfires/code.js
@@ -1,22 +1,6 @@
 // 21/2/14
 
 
-function Solution2(){
-	
-	var total=0;
-	var previous=1;
-	var current=2;
-	var sum = 0;
-	while (current <4000000){
-		if(current%2===0){
-			total=total+current;
-		}
-		sum=previous+current;
-		previous=current;
-		current=sum;
-	}
-	document.getElementById("solution2").innerHTML="Sum of even Fibonacci = "+total;
-}
 
 function Solution5(){
 	var str = prompt("Please enter a sentence", "The quick brown fox jumped over the lazy dog");
@@ -28,5 +12,21 @@ function Solution5(){
 		}
 	}
   alert("The longest word is "+num+" characters long");
+	
+}
+
+
+function Solution6(){
+	
+var str = prompt("Please enter a sentence", "I'm a little tea pot");
+	var titleCase = "";
+	var strArray = str.split(" ");
+	for (pos in strArray){
+		if(pos>0){
+			titleCase+=" ";
+		}
+		titleCase=titleCase+strArray[pos][0].toUpperCase()+strArray[pos].substring(1).toLowerCase();
+	}
+  alert(titleCase);
 	
 }
