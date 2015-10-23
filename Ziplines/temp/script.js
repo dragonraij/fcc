1004bdf0c11704d2ba666752188432284b348c73
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-10-23 21:50:57 +0000

    Add toggle functionality to timer

--- Ziplines/temp/script.js
+++ Ziplines/temp/script.js
@@ -1,21 +1,47 @@
-var pomo=0;
+var pomo=25;
+var timerOn=false;
+
 
 function PomoPlus() {
     pomo++;
+    document.getElementById("Pomotime").innerHTML = pomo;
     document.getElementById("time").innerHTML = pomo;
 }
 
 function PomoMinus() {
     pomo--;
+    document.getElementById("Pomotime").innerHTML = pomo;
     document.getElementById("time").innerHTML = pomo;
+     if (timerOn) {
+        timerOn=false;
+    }else {
+        timerOn=true;
+    }
+    console.log(timerOn);
+    
 }
 
 function BreakPlus() {
     pomo++;
     document.getElementById("break").innerHTML = pomo;
+     if (timerOn) {
+        timerOn=false;
+    }else {
+        timerOn=true;
+    }
+    console.log(timerOn);
 }
 
 function BreakMinus() {
     pomo--;
     document.getElementById("break").innerHTML = pomo;
+}
+
+function ToggleTimer(){
+    if (timerOn) {
+        timerOn=false;
+    }else {
+        timerOn=true;
+    }
+    console.log(timerOn);
 }
