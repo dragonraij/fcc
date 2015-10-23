0868f2c28b0cfb9f52ee249b8405b7b6d87a051a
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-10-23 03:48:33 +0000

    Add second button control

--- Ziplines/temp/script.js
+++ Ziplines/temp/script.js
@@ -1,3 +1,21 @@
-function tion() {
-    document.getElementById("time").style.color = "red";
+var pomo=0;
+
+function PomoPlus() {
+    pomo++;
+    document.getElementById("time").innerHTML = pomo;
+}
+
+function PomoMinus() {
+    pomo--;
+    document.getElementById("time").innerHTML = pomo;
+}
+
+function BreakPlus() {
+    pomo++;
+    document.getElementById("break").innerHTML = pomo;
+}
+
+function BreakMinus() {
+    pomo--;
+    document.getElementById("break").innerHTML = pomo;
 }
