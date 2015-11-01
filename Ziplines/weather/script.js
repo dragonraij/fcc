55b99ef26694e040b5eae1ddd6edbc5a88131c94
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-11-02 01:34:16 +1300

    Test input from weather API

--- Ziplines/weather/script.js
+++ Ziplines/weather/script.js
@@ -2,28 +2,37 @@ var cntry =0;
 var hi=0;
 
 //declare variables
-var city="", country ="";
+var city="", country ="", temp=0;
 
 
 $( document ).ready(function() {
     console.log( "ready!" );
 
 //get location data    
-$.getJSON("http:///www.ipinfo.io/", function(response){
-    console.log("Jason Data");
-    console.log(response);
+$.getJSON("http:///www.ipinfo.io/", null, function(response){
+    
+    //store location data
     city=response.city;
     country=response.country;
     cntry=response;
-});
 
-//get weather data
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Auckland,NZ&APPID=aae4a1f36a91da5662b14523e312a089", function(json){
-	console.log("Weather is");
-	console.log(json);
-	hi=json;
+    //update display
+	document.getElementById("city").innerHTML = city+", "+country;
+	
 
-});
+	//get weather data
+	$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&units=metric&APPID=aae4a1f36a91da5662b14523e312a089", null,function(json){
+		hi=json;
+		document.getElementById("weather").innerHTML = json.weather[0].description;	
+	});
+
+
+
+	});
+
+	
+
+
 });
 
 
