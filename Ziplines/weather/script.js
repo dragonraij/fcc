48be59050ee2b31bca07f1744cf17bb7912e2e55
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-11-01 20:59:06 +1300

    Get Weather and location info using JSON

--- Ziplines/weather/script.js
+++ Ziplines/weather/script.js
@@ -1,24 +1,38 @@
-<<<<<<< HEAD
-$(document).ready(function (){
-    
-            
-            $.get("http://ipinfo.io", function(response) {
-    		console.log(response.ip, response.country, response.city);
-			document.getElementById("weather").innerHTML=response.city;
-			}, "jsonp");
-      
-
-    
-=======
+var cntry =0;
 var hi=0;
+
+//declare variables
+var city="", country ="";
+
+
 $( document ).ready(function() {
     console.log( "ready!" );
->>>>>>> origin/master
-    
-$.getJSON("ipinfo.io/8.8.8.8", function(response){
+
+//get location data    
+$.getJSON("http:///www.ipinfo.io/", function(response){
     console.log("Jason Data");
     console.log(response);
+    city=response.city;
+    country=response.country;
+    cntry=response;
+});
+
+//get weather data
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Auckland,NZ&APPID=aae4a1f36a91da5662b14523e312a089", function(json){
+	console.log("Weather is");
+	console.log(json);
+	hi=json;
+
 });
 });
 
 
+/*$.get
+
+("http://api.openweathermap.org/data/2.5/weather?q=Modesto&APPID=aae4a1f36a91da5662b14523e312a089", function
+
+(response){
+    console.log("Jason Data");
+    console.log(response); hi=response;
+});
+});*/
