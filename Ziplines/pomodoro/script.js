99ac460753883518d5c96b1985381bf3881f8a91
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-11-01 16:23:12 +1300

    update pomodoro app

--- Ziplines/pomodoro/script.js
+++ Ziplines/pomodoro/script.js
@@ -1,22 +1,25 @@
-var pomodoro=0;
-var breaka=0;
+var pomodoro=25;
+var breaka=5;
 var timer=0;
 
 function minusBreak (){
-
+	document.getElementById("break").innerHTML = "New text!";
 }
 
 
 function plusBreak (){
-	
+		document.getElementById("break").innerHTML = "New text!";
+}
 }
 
 
 function minusPomo (){
-	
+		document.getElementById("break").innerHTML = "New text!";
+}
 }
 
 
 function plusPomo (){
-	
+		document.getElementById("break").innerHTML = "New text!";
+}
 }
--- Ziplines/weather/script.js
+++ Ziplines/weather/script.js
@@ -1,7 +1,12 @@
 $(document).ready(function (){
     
-            document.getElementById("weather").innerHTML="Auckland";
+            
+            $.get("http://ipinfo.io", function(response) {
+    		console.log(response.ip, response.country, response.city);
+			document.getElementById("weather").innerHTML=response.city;
+			}, "jsonp");
       
+
     
     
 });
