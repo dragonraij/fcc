bb922ca0cebe77067c696514c63abd090cb8387d
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-11-02 01:57:44 +0000

    Allow temperature conversion

--- Ziplines/weather/script.js
+++ Ziplines/weather/script.js
@@ -1,9 +1,9 @@
 var cntry =0;
-var hi=0;
+
 
 //declare variables
-var city="", country ="", temp=0;
-
+var city="", country ="", temp=0; degrees="celcius";
+var weather="";
 
 $( document ).ready(function() {
     console.log( "ready!" );
@@ -22,9 +22,9 @@ $.getJSON("http:///www.ipinfo.io/", null, function(response){
 
 	//get weather data
 	$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&units=metric&APPID=aae4a1f36a91da5662b14523e312a089", null,function(json){
-		hi=json;
+		weather=json;
 		document.getElementById("weather").innerHTML = json.weather[0].description;	
-	    document.getElementById("icon").src="www.xyz.com/"+hi.url+".png";
+	    document.getElementById("icon").src="www.xyz.com/"+weather.url+".png";
 	    
 	});
 
@@ -37,6 +37,16 @@ $.getJSON("http:///www.ipinfo.io/", null, function(response){
 
 });
 
+
+function changeTemp(){
+    if(degrees="celcius"){
+        temp= (temp *  9/5) + 32;
+        degrees= "fahrenheit";
+    }else{
+        
+        degrees="celcius";
+    }
+}
 
 /*$.get
 
