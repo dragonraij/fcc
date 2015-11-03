308d60919fe40a507652e792d1de6374f635ea05
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-11-03 21:36:58 +1300

    Change background image depending on weather conditions

--- Ziplines/weather/script.js
+++ Ziplines/weather/script.js
@@ -24,11 +24,49 @@ $.getJSON("http:///www.ipinfo.io/", null, function(response){
 	$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+city+","+country+"&units=metric&APPID=aae4a1f36a91da5662b14523e312a089", null,function(json){
 		weather=json;
 		temp=json.main.temp;
-        document.getElementById("weather").innerHTML = json.weather[0].description;
-        document.getElementById("temp").innerHTML = temp+"&#x2103;";  //faren 2109	
-	    document.getElementById("icon").src="http://openweathermap.org/img/w/"+weather.weather[0].icon+".png";
+        document.getElementById("weather").innerHTML = json.weather[0].description; // verbal description of current weather
+        document.getElementById("temp").innerHTML = temp+"&#x2103;";  //get current temperature in celcius
+        document.getElementById("icon").src="http://openweathermap.org/img/w/"+weather.weather[0].icon+".png";  //display current weather icon
 	    
-	});
+        //select image id depending on conditions
+        
+        var imgURL = "";
+        switch(weather.weather[0].main) {
+    case "Thunderstorm":
+        imgURL = "0B2FfVLw7EUDXRTFRelBfcm5OYmM";
+        break;
+    case "Drizzle":
+        imgURL = "0B2FfVLw7EUDXb1dBcVhwSzhwNVk";
+        break;
+    case "Rain":
+        imgURL = "0B2FfVLw7EUDXT24xcTNjX3c4Zkk";
+        break;
+    case "Snow":
+        imgURL = "0B2FfVLw7EUDXR0dZdzVqcEt4NUE";
+        break;
+    case "Atmosphere":
+        imgURL = "0B2FfVLw7EUDXUkx1TzVQTy1hSEU";
+        break;
+    case "Clear":
+        imgURL = "0B2FfVLw7EUDXY3FKcDMxSm40Z2s";
+        break;
+    case "Clouds":
+        imgURL = "0B2FfVLw7EUDXbDllSlc0aVd1b1U";
+        break;
+    case "Extreme":
+        imgURL = "0B2FfVLw7EUDXTzRnX3gxWUlVWFE";
+        break;
+
+    default:
+        imgURL = "0B2FfVLw7EUDXVl9yTkowS2wxZEk";
+}
+	
+    //Retrieve the image from server using selected id
+        document.body.style.background = "url('http://drive.google.com/uc?export=view&id="+imgURL+"') no-repeat center center fixed";
+        document.getElementById("mainBody").style.backgroundSize="cover";
+
+
+    });
 
 
 
